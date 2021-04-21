1dcd7ba1b51e923530f8246842e79ec48641d722
Cameron <cameron.meyer02@gmail.com>
2021-04-21 17:36:12 -0500

    Output to File

--- src/dataGen.java
+++ src/dataGen.java
@@ -1,85 +1,128 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 // Cameron Meyer
 // Assignment 5 - Exercise 6.9
 // CS 4361.001
-
-
-import java.awt.*;
-import java.awt.event.*;
-import java.nio.file.FileSystemNotFoundException;
 
 
 public class dataGen
 {
 	public static void main(String [] args)
 	{
-		int numSteps = 1;
-		int stepLength = 2;
-		int stepWidth = 5;
-		int stepHeight = 2;
+		int numSteps = 0;
+		int stepLength = 0;
+		int stepWidth = 0;
+		int stepHeight = 0;
+		
+        if(args.length == 4)
+        {
+            try
+            {
+            	numSteps = Integer.parseInt(args[0]);
+            	stepLength = Integer.parseInt(args[1]);
+            	stepWidth = Integer.parseInt(args[2]);
+            	stepHeight = Integer.parseInt(args[3]);
+            }
+            catch(Exception E)
+            {
+            	System.out.println("Could not convert all arguments to integers. Please ensure numSteps, stepLength, stepWidth, stepHeight are all integers.");
+            	System.exit(-1);
+            }
+        }
+        else
+        {
+        	System.out.println("Incorrect number of arguments. Expected 4 arguments, but found " + args.length + ". Please include only numSteps, stepLength, stepWidth, stepHeight.");
+        	System.exit(-1);
+        }
+
 		
 		if(numSteps > 0)
 		{
-			// Create the 4 vertices that make up the base
-			System.out.println("1 0 0 0");
-			System.out.println("2 " + (stepLength*numSteps) + " 0 0");
-			System.out.println("3 " + (stepLength*numSteps) + " " + stepWidth + " 0");
-			System.out.println("4 0 " + stepWidth + " 0");
-			
-			for(int i = 1; i <= numSteps; i++)
+			try
 			{
-				// Create 4 vertices at a time for the top of each step
-				int innerStepLength = (numSteps - i) * stepLength; 
-				int outerStepLength = (numSteps - i) * stepLength + stepLength; 
-				//float stepHeight = i*(height/numSteps);
-				System.out.println(i*4+1 + " " + innerStepLength + " " + 0 + " " + (stepHeight*i));
-				System.out.println(i*4+2 + " " + outerStepLength + " " + 0 + " " + (stepHeight*i));
-				System.out.println(i*4+3 + " " + outerStepLength + " " + stepWidth + " " + (stepHeight*i));
-				System.out.println(i*4+4 + " " + innerStepLength + " " + stepWidth + " " + (stepHeight*i));
-			}
-			
-			System.out.println("Faces:");	
-			
-			// Bottom face
-			System.out.println("1 4 3 2.");
-			
-			// First stair front
-			System.out.println("2 3 7 6.");
-			
-			// First stair top
-			System.out.println("5 6 7 8.");
-			
-			if(numSteps > 1)
-			{
-				for(int i = 2; i <= numSteps; i++)
+				File output = new File("stairsObj.dat");
+				if (output.createNewFile())
 				{
-					// Stair Front
-					System.out.println((4*i-3) + " " + (4*i) + " " + (4*i+3) + " " + (4*i+2) + ".");
-					
-					// Stair Top
-					System.out.println((4*i+1) + " " + (4*i+2) + " " + (4*i+3) + " " + (4*i+4) + ".");
+					System.out.println("File created: " + output.getName());
 				}
+				else
+				{
+					System.out.println("File \"" + output.getName() + "\" already exists.");
+				}
+				
+			    BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+			    
+			    // Create the 4 vertices that make up the base
+			    writer.write("1 0 0 0\n");
+			    writer.write("2 " + (stepLength*numSteps) + " 0 0\n");
+			    writer.write("3 " + (stepLength*numSteps) + " " + stepWidth + " 0\n");
+			    writer.write("4 0 " + stepWidth + " 0\n");
+			    
+			    for(int i = 1; i <= numSteps; i++)
+				{
+					// Create 4 vertices at a time for the top of each step
+					int innerStepLength = (numSteps - i) * stepLength; 
+					int outerStepLength = (numSteps - i) * stepLength + stepLength; 
+
+					writer.write(i*4+1 + " " + innerStepLength + " " + 0 + " " + (stepHeight*i) + "\n");
+					writer.write(i*4+2 + " " + outerStepLength + " " + 0 + " " + (stepHeight*i) + "\n");
+					writer.write(i*4+3 + " " + outerStepLength + " " + stepWidth + " " + (stepHeight*i) + "\n");
+					writer.write(i*4+4 + " " + innerStepLength + " " + stepWidth + " " + (stepHeight*i) + "\n");
+				}
+				
+			    writer.write("Faces:\n");	
+				
+				// Bottom face
+			    writer.write("1 4 3 2.\n");
+				
+				// First stair front
+			    writer.write("2 3 7 6.\n");
+				
+				// First stair top
+				writer.write("5 6 7 8.\n");
+				
+				if(numSteps > 1)
+				{
+					for(int i = 2; i <= numSteps; i++)
+					{
+						// Stair Front
+						writer.write((4*i-3) + " " + (4*i) + " " + (4*i+3) + " " + (4*i+2) + ".\n");
+						
+						// Stair Top
+						writer.write((4*i+1) + " " + (4*i+2) + " " + (4*i+3) + " " + (4*i+4) + ".\n");
+					}
+				}
+				
+				// Back face
+				writer.write("1 " + (4*numSteps+1) + " " + (4*numSteps+4) + " 4.\n");
+				
+				// Left Side
+				writer.write("1 2");
+				for(int i = 1; i <= numSteps; i++)
+				{
+					int point = 6 + (i - 1) * 4;
+					writer.write(" " + point + " " + (point-1));
+				}
+				writer.write(".\n");
+				
+				// Right Side
+				writer.write("3 4");
+				for(int i = 1; i <= numSteps; i++)
+				{
+					int point = 8 + (numSteps - i) * 4;
+					writer.write(" " + point + " " + (point-1));
+				}
+				writer.write(".\n");
+			    
+			    writer.close();
 			}
-			
-			// Back face
-			System.out.println("1 " + (4*numSteps+1) + " " + (4*numSteps+4) + " 4.");
-			
-			// Left Side
-			System.out.print("1 2");
-			for(int i = 1; i <= numSteps; i++)
+			catch(IOException e)
 			{
-				int point = 6 + (i - 1) * 4;
-				System.out.print(" " + point + " " + (point-1));
+				System.out.println("An error occurred while writing to the output file.");
 			}
-			System.out.print(".\n");
-			
-			// Right Side
-			System.out.print("3 4");
-			for(int i = 1; i <= numSteps; i++)
-			{
-				int point = 8 + (numSteps - i) * 4;
-				System.out.print(" " + point + " " + (point-1));
-			}
-			System.out.print(".\n");
 		}
 	}
 }
